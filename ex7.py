6ce662b0d6f340b771533730e97c0a5b5997f888
vikaskumar94 <vikaskumar.vsk@gmail.com>
2017-07-28 15:34:06 +0530

    comments added

---
+++ ex7.py
@@ -0,0 +1,28 @@
+# a string is printed.
+print "Mary had a little lamb."
+# 'snow' string is assigned to string variable '%s' inside a string.
+print "Its fleece was white as %s." % 'snow'
+# string is printed.
+print "And everywhere that Mary went."
+# '.' is printed x10 [like this: ..........]
+print "." * 10 # what'd that do?
+
+# variables are assigned string.
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# eatch that comma at the end. try removing it to see what  happens.
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
+# when comma is removed bot are printed in different lines
+# otherwise both are separated by a space.
