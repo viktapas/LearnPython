e289acc848d6116c148e3ceb098c5a401db628ba
vikaskumar94 <vikaskumar.vsk@gmail.com>
2017-07-29 18:46:12 +0530

    bug fix + comment added

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+# to run this program you also need to give input(in this case three inputs) viz. $python exe13.py 1 2 3
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
