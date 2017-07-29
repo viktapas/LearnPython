0c03208eecb5a68b63ad71ce37eb44b88c55f8e1
vikaskumar94 <vikaskumar.vsk@gmail.com>
2017-07-29 15:30:32 +0530

    better commit

--- ex11.py
+++ ex11.py
@@ -1,6 +1,9 @@
-age = raw_input("How old are you? ")
-height = raw_input("How tall are you? ")
-weight = raw_input("How much do you weight? ")
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weight?",
+weight = raw_input()
 
-print "So, you're %r old, %r tall and %r heavy." %(
-	age, height, weight)
+print "So, you're %r old, %r tall and %r heavy." % (
+	age, height, weight
