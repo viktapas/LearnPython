229e0ee9b30f004f055fa945dea37dd71a4c65a5
vikaskumar94 <vikaskumar.vsk@gmail.com>
2017-07-29 13:05:10 +0530

    exercise complete

--- ex9.py
+++ ex9.py
@@ -1,11 +1,13 @@
 
 
 days = "Mon Tue Wed Thu Fri Sat Sun"
+#To start printing Jan in next line use \n befor Jan like \nJan
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
 print "Here are the days:", days
 print "Here are the months:", months
 
+# Anything written between two """ are treated as string. See below
 print """
 There's something going on here.
 With the three double-quotes.
