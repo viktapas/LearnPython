2e0c6fe65ed63a343d4319b437381b3bfa2d2a9e
vikaskumar94 <vikaskumar.vsk@gmail.com>
2017-07-29 15:19:26 +0530

    first commit

--- ex10.py
+++ ex10.py
@@ -3,7 +3,7 @@ persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
 fat_cat = """
-I'll do a list.
+I'll do a list:
 \t* Cat food
 \t* Fishies
 \t* Catnip\n\t* Grass
@@ -13,3 +13,9 @@ print tabby_cat
 print persian_cat
 print backslash_cat
 print fat_cat
+
+
+# extra workout
+#while True:
+#	for i in ["/","-","|","\\","|"]:
+#		print "%s\r" %i,
