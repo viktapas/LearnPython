bf8ae9f2320acf76476f40a02deec6ebee385315
vikaskumar94 <vikaskumar.vsk@gmail.com>
2017-07-30 16:29:06 +0530

    shortened script

--- ex17.py
+++ ex17.py
@@ -3,27 +3,26 @@ from os.path import exists
 
 script, from_file, to_file = argv
 
-print "Copying from %s to %s" %(from_file, to_file)
-
-in_file =open(from_file)
-indata = in_file.read()
+#print "Copying from %s to %s" %(from_file, to_file)
 
 # we could do these two on one line too, how?
 
 #in_file = open(from_file)
 #indata = in_file.read()
 
-indata = open(from_file).read()
+#indata = open(from_file).read()
 
 
-print "The input file is %d bytes long" % len(indata)
+#print "The input file is %d bytes long" % len(indata)
 
-print "Does the output file exists? %r" % exists(to_file)
-raw_input('Hit ENTER to copy file, CTRL+C to abort.')
+#print "Does the output file exists? %r" % exists(to_file)
+#raw_input('Hit ENTER to copy file, CTRL+C to abort.')
 
-out_file = open(to_file, 'w')
-out_file.write(indata)
+#out_file = open(to_file, 'w')
+#out_file.write(indata)
+
+out_file = open(to_file, 'w').write(open(from_file).read())
 
 print "Alright, all done!"
 
-out_file.close()
+#out_file.close()
