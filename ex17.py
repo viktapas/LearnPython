b0e8172da35ed7cd07fd3dcd871b0f96643b27ff
vikaskumar94 <vikaskumar.vsk@gmail.com>
2017-07-30 15:54:33 +0530

    initial commit

---
+++ ex17.py
@@ -0,0 +1,29 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print "Copying from %s to %s" %(from_file, to_file)
+
+in_file =open(from_file)
+indata = in_file.read()
+
+# we could do these two on one line too, how?
+
+#in_file = open(from_file)
+#indata = in_file.read()
+
+indata = open(from_file).read()
+
+
+print "The input file is %d bytes long" % len(indata)
+
+print "Does the output file exists? %r" % exists(to_file)
+raw_input('Hit ENTER to copy file, CTRL+C to abort.')
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print "Alright, all done!"
+
+out_file.close()
