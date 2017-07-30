ac574ffc02daab636876c41e9f9fb88d73c24ec2
vikaskumar94 <vikaskumar.vsk@gmail.com>
2017-07-30 21:09:37 +0530

    comments added

--- ex19.py
+++ ex19.py
@@ -1,3 +1,7 @@
+# make a function 'cheese_and_crackers' using def for define
+# cheese_and_crackers is function
+# cheese_count and boxes_of_crackers are arguments
+# arguments' values are not passed directly
 def cheese_and_crackers(cheese_count, boxes_of_crackers):
 	print "You have %d cheeses!" % cheese_count
 	print "You have %d boxes of crackers!" % boxes_of_crackers
@@ -6,18 +10,23 @@ def cheese_and_crackers(cheese_count, boxes_of_crackers):
 
 
 print "We can just give the function numbers directly:"
+# arguments' values are passed directly to the function
 cheese_and_crackers(20,30)
 
 
 print "OR, we can use variables from our script:"
+# arguments' values are passed using function's variables
 amount_of_cheese = 10
 amount_of_crackers = 50
 
+# cheese_and_crackers runs
 cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
 
 print "We can even do math inside too:"
+# arguments' vlaues are passed directly but with math
 cheese_and_crackers(10 + 20, 5 + 6)
 
 print "And we can combine the two, variables and math:"
+# arguments' values are passed using variables and math combined
 cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
