77fba8053f90457db2714d7a638630ad6e783e1e
vikaskumar94 <vikaskumar.vsk@gmail.com>
2017-08-01 06:08:32 +0530

    comments added

--- ex20.py
+++ ex20.py
@@ -1,20 +1,26 @@
+# imports argv module from sys
 from sys import argv
 
+# arguments to passed to argv
 script, input_file = argv
 
+# define a function
 def print_all(f):
 	print f.read()
 
 def rewind(f):
 	f.seek(0)
 
+# function print_a_line takes two arguments
 def print_a_line(line_count, f):
 	print line_count, f.readline()
 
+#current_file is open input_file
 current_file = open(input_file)
 
 print "First let's print the whole file:\n"
 
+# current_file is argument of print_all function
 print_all(current_file)
 
 print("Now let's rewind, kind of like a tape.")
@@ -23,11 +29,13 @@ rewind(current_file)
 
 print("Let's print three lines:")
 
-current_line =1
+# 1 is assigned to current_line
+current_line = 1
 print_a_line(current_line, current_file)
 
-current_line = current_line +1
+# current_line is incremented by 1 over its previous value
+current_line = current_line + 1
 print_a_line(current_line, current_file)
 
-current_line = current_line +1
+current_line = current_line + 1
 print_a_line(current_line, current_file)
