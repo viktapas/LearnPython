3efc6a3c530810c5e107fab9c8ef066038d2b4c6
vikaskumar94 <vikaskumar.vsk@gmail.com>
2017-08-02 16:18:33 +0530

    initial commit

---
+++ ex30.py
@@ -0,0 +1,24 @@
+people = 300
+cars = 115
+buses = 115
+
+
+if cars > people:
+	print "We should take the cars."
+elif cars < people:
+	print "We should not take the cars."
+else:
+	print "We can't deccide."
+
+if buses > cars:
+	print"That's too many buses."
+elif buses < cars:
+	print "May be we could take the busese."
+else:
+	print "We still can't decide."
+
+if people > buses:
+	print "Alright, let's just take the buses."
+else:
+	print "Fine, let's stay home then."
+
