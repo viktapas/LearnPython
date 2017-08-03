2e7dbdb5db7a69d0f61f7539960316084c80b55a
vikaskumar94 <vikaskumar.vsk@gmail.com>
2017-08-03 12:20:16 +0530

    while-loop changed to a function that can be called.

--- ex33.py
+++ ex33.py
@@ -1,14 +1,18 @@
 i = 0
 numbers = []
 
-while i < 6:
-	print "At the top i is %d" % i
-	numbers.append(i)
+# changed while-loop into function that can be called
+def list(i):
+	while i < 6:
+		print "At the top i is %d" % i
+		numbers.append(i)
 
-	i = i +1
-	print "Numbers now: ", numbers
-	print "At the bottom i is %d" % i
+		i = i +1
+		print "Numbers now: ", numbers
+		print "At the bottom i is %d" % i
 
+# function called
+print list(i)
 
 print "The numbers: "
 
