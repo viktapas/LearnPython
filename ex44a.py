2092db0ee526f151cb500ffdf12d4fdb6a38b46c
vikaskumar94 <vikaskumar.vsk@gmail.com>
2017-08-20 17:30:34 +0530

    initial commit

---
+++ ex44a.py
@@ -0,0 +1,14 @@
+# ex44a.py
+class Parent(object):
+
+	def implicit(self):
+		print "PARENT implicit()"
+
+class Child(Parent):
+	pass
+
+dad = Parent()
+son = Child()
+
+dad.implicit()
+son.implicit()
---
+++ ex44b.py
@@ -0,0 +1,16 @@
+# ex44b.py
+class Parent(object):
+
+	def override(self):
+		print "PARENT override()"
+
+class Child(Parent):
+
+	def override (self):
+		print "CHILD override()"
+
+dad = Parent()
+son = Child()
+
+dad.override()
+son.override()
