27f13cf95551db9e28111cfb17e3dc6b9406bed3
vikaskumar94 <vikaskumar.vsk@gmail.com>
2017-09-11 20:46:55 +0530

    comments added

--- UdemyPY/HelloWorld.py
+++ UdemyPY/HelloWorld.py
@@ -1,6 +1,7 @@
 from Tkinter import * #use 'tkinter' if you're using python 3.x
 
 root = Tk()
-Label (root, text = "Hello World!").pack()
-root.mainloop()
+Label (root, text = "Hello World!").pack() # .pack is essential to display the text on the screen...
+					   # ...Without which no text will be displayed
+root.mainloop() # this creates infinite loop which prevents windows from getting closed by itself
 
