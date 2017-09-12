a58274c8c1df5d95ba3994db5ac1c1b9cdbe52ee
vikaskumar94 <vikaskumar.vsk@gmail.com>
2017-09-12 12:35:37 +0530

    error fixed/ working 100%

---
+++ UdemyPY/clock.py
@@ -0,0 +1,35 @@
+from Tkinter import * #Tkinter libarary used to create GUI application
+#from Tkinter 
+import ttk
+import tkFont # import 'font' instead in python3
+import time
+import datetime
+
+def quit(*args): 
+    root.destroy()  #closes the window
+
+def clock_time():
+	time = datetime.datetime.now() #gets current time
+	time = (time.strftime("%H:%M:%S")) #time format
+
+	txt.set(time)
+
+	root.after(1000,clock_time)
+ #these variables are for the window screen
+ #creates widgets
+root = Tk()
+root.attributes("-fullscreen", False)
+root.config(background = 'black') # use 'configure' instead in python3
+root.bind("x", quit)
+root.after(1000, clock_time)
+
+#these variables are for the clock itself
+fnt = tkFont.Font(family = 'Helvetics', size = 120, weight = 'bold')
+txt = StringVar()
+lbl = ttk.Label(root, textvariable = txt, font = fnt, foreground = "white", background = "black")
+lbl.place(relx = 0.5, rely = 0.5, anchor = CENTER) #aling the clock to the center
+
+root.mainloop()
+
+    
+
